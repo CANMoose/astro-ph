1a7aadeed0c00c611cd0c7702c5c46a37720ed30
CANMoose <remphys@gmail.com>
2015-07-06 22:04:24 -0400

    Update to remove some swp files from the repo

--- astrophbot.py
+++ astrophbot.py
@@ -1,34 +1,39 @@
 import astroph as aph
-import sched, time
+import sys
 
-def create_email(keywords,email):
+def create_email(my_keywords,email):
+    print "Running astrophbot for %s\n" % (email)
 
+    print "Checking arxiv...\n"
     artList = aph.get_artlist()
+    print "Arxiv downloaded, checking for the following keywords: "
+    for keyword in my_keywords:
+        print keyword
+    print '\n'
+    
     kwdict = aph.find_in_abstracts(artList,my_keywords)
-
+    print kwdict
+    
     ct=0
     body ="astroph-bot has searched the abstracts from today's astro-ph rss feed for the following keywords:"\
         +"\r\n"+",".join(my_keywords)+"\r\n\r\n"
     for n,key in enumerate(kwdict.keys()):
-            if len(test[key]) > 0:
+            if len(kwdict[key]) > 0:
                 ct = ct + len(kwdict[key])
                 body = body + "\r\n".join((["The following articles have the keyword: "\
                     +key+"\r\n","\r\n".join([artList[x].title + artList[x].link for x in kwdict[key]])]))\
                     + "\r\n\r\n"
+     
+    print "Sending email to %s...\n" % (email)
     
-    if ct > 0: 
-        #send_email(str(sys.argv[1]),body)
-        aph.send_email(email,body)
-
+    #send_email(str(sys.argv[1]),body)
+    #aph.send_email(email,body)
 
 if __name__ == '__main__':
-    s = sched.schedular(time.time, time.sleep)
 
-    keywords = ['IMF','Initial Mass Function', 'Mass Function']
+    #ENTER YOUR KEYWORDS AND EMAIL HERE#
+    keywords = ['asdfasdf','asdfsdfa']
     email = 'meyer@astro.utoronto.ca'
-
-    nsec_day = 86400
-
-    s.enter(60,1, create_email(keywords,email))
-
-
+    ####################################
+    
+    create_email(keywords,email)
